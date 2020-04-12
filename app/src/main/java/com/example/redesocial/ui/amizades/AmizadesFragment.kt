63bdc6a19c71657fc357b582730f7c873cf3db37
Lucas Martins <lucas.martins@ed.company>
2020-04-12 10:32:45 -0300

    Adicionada recyclerView para fragment amizades

--- app/src/main/java/com/example/redesocial/ui/amizades/AmizadesFragment.kt
+++ app/src/main/java/com/example/redesocial/ui/amizades/AmizadesFragment.kt
@@ -6,33 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.redesocial.R
+import com.example.redesocial.adapters.PessoaAdapter
+import kotlinx.android.synthetic.main.fragment_amizades.*
 
 
-/**
- * A simple [Fragment] subclass.
- */
 class AmizadesFragment : Fragment() {
-
-    private lateinit var amizadesViewModel: AmizadesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        amizadesViewModel =
-            ViewModelProviders.of(this).get(AmizadesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_amizades, container, false)
-        val textView: TextView = root.findViewById(R.id.text_amizades)
-        amizadesViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_amizades, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configurarRecyclerView()
+    }
 
+    fun configurarRecyclerView()
+    {
+        listagemAmizades.layoutManager = LinearLayoutManager(activity)
+        listagemAmizades.adapter = PessoaAdapter()
+    }
 }
