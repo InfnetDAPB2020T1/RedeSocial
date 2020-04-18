e04564942119c25d8bb9befad53232b93d6db92b
Lucas Martins <lucas.martins@ed.company>
2020-04-18 15:18:23 -0300

    Adicionada verificação de usuário no login

--- app/src/main/java/com/example/redesocial/MainActivity.kt
+++ app/src/main/java/com/example/redesocial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.redesocial
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import me.ibrahimsn.particle.ParticleView
 
@@ -17,14 +18,52 @@ class MainActivity : AppCompatActivity() {
         particleView = findViewById(R.id.particleView)
 
         btnLogin.setOnClickListener{
-            var i = Intent(this, TelaPrincipal::class.java)
-            startActivity(i)
+
+            var username = usernameLogin.text.toString()
+            var password = passwordLogin.text.toString()
+
+            if(verificaCampoPreenchido(username) && verificaCampoPreenchido(password))
+            {
+                if(verificaUsuario(username,password))
+                {
+                    var i = Intent(this, TelaPrincipal::class.java)
+                    startActivity(i)
+                }
+                else
+                {
+                    Toast.makeText(this,"Usuário não cadastrado!",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Preencha corretamente os campos!",Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         btnIrParaCadastro.setOnClickListener{
             var i = Intent(this, Cadastro::class.java)
             startActivity(i)
         }
+    }
+
+    fun verificaUsuario(username : String, password : String) : Boolean
+    {
+        var usuarioRegistrado = "user"
+        var senhaRegistrada = "senha"
+
+        if(username.trim() == usuarioRegistrado && password.trim() == senhaRegistrada)
+            return true;
+
+        return false
+    }
+
+    fun verificaCampoPreenchido(param : String) : Boolean
+    {
+        if(param != null && param.trim().isNotEmpty())
+            return true
+
+        return false
     }
 
     override fun onResume() {
