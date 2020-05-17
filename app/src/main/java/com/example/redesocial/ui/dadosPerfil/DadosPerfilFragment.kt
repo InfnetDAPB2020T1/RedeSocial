6f684dfd894b1c862ff5a1db3b1653139c574294
Lucas Martins <lucas.martins@ed.company>
2020-05-17 16:49:55 -0300

    Adicionado forma de sair da conta

--- app/src/main/java/com/example/redesocial/ui/dadosPerfil/DadosPerfilFragment.kt
+++ app/src/main/java/com/example/redesocial/ui/dadosPerfil/DadosPerfilFragment.kt
@@ -1,6 +1,7 @@
 package com.example.redesocial.ui.dadosPerfil
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,8 +10,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.redesocial.MainActivity
 
 import com.example.redesocial.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_dados_perfil.*
 
 
 /**
@@ -26,6 +30,16 @@ class DadosPerfilFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_dados_perfil, container, false)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_logoff.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            var intent = Intent(activity!!,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
