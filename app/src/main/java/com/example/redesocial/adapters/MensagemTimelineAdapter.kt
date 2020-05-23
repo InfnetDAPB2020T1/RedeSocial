6e6ea1c349ca95cf58bc6fcd8d99090e11a4ac84
Lucas Martins <lucas.martins@ed.company>
2020-05-22 22:32:00 -0300

    Adicionada lista no fragmento timeline

---
+++ app/src/main/java/com/example/redesocial/adapters/MensagemTimelineAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.redesocial.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.redesocial.R
+import com.example.redesocial.models.MensagemTimeline
+import com.google.android.material.imageview.ShapeableImageView
+
+class MensagemTimelineAdapter(listaMensagens : List<MensagemTimeline>) : RecyclerView.Adapter<MensagemTimelineAdapter.MensagemTimelineViewHolder>() {
+
+    var listaMensagens = listaMensagens
+
+    class MensagemTimelineViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        var nomeCard = itemView.findViewById<TextView>(R.id.username_timeline)
+        var avatarCard = itemView.findViewById<ShapeableImageView>(R.id.foto_lista_timeline)
+        var textoCard = itemView.findViewById<TextView>(R.id.mensagem_timeline)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MensagemTimelineViewHolder {
+        val card = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.timeline_item, parent, false)
+
+        return MensagemTimelineViewHolder(card)
+    }
+
+    override fun getItemCount() = listaMensagens.size
+
+    override fun onBindViewHolder(holder: MensagemTimelineViewHolder, position: Int) {
+
+        holder.nomeCard.text = listaMensagens[position].username
+        holder.avatarCard.setImageResource(listaMensagens[position].foto)
+        holder.textoCard.text = listaMensagens[position].textoMensagem
+    }
+}
--- app/src/main/java/com/example/redesocial/ui/timeline/TimelineFragment.kt
+++ app/src/main/java/com/example/redesocial/ui/timeline/TimelineFragment.kt
@@ -9,8 +9,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.redesocial.R
+import com.example.redesocial.adapters.MensagemTimelineAdapter
+import com.example.redesocial.models.MensagemTimeline
+import kotlinx.android.synthetic.main.fragment_timeline.*
 
 
 /**
@@ -18,21 +22,42 @@ import com.example.redesocial.R
  */
 class TimelineFragment : Fragment() {
 
-    private lateinit var timelineViewModel: TimelineViewModel
+    var listaMensagens = mutableListOf<MensagemTimeline>(
+        MensagemTimeline("Lucas",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Martins",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Pedro",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Paulo",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Cecilia",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Santos",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Seu Madruga",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        MensagemTimeline("Rocky Balboa",R.drawable.userquadrado,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        timelineViewModel =
-            ViewModelProviders.of(this).get(TimelineViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_timeline, container, false)
-        val textView: TextView = root.findViewById(R.id.text_timeline)
-        timelineViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configurarRecyclerView()
+    }
 
+    fun configurarRecyclerView() {
+
+        if(!listaMensagens.isNullOrEmpty())
+        {
+            lista_timeline.layoutManager = LinearLayoutManager(activity)
+            lista_timeline.adapter = MensagemTimelineAdapter(listaMensagens)
+        }
+        else
+        {
+            lista_timeline.visibility = View.GONE
+            empty_view_timeline.visibility = View.VISIBLE
+        }
+    }
 }
